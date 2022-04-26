88d5586f50f975b157e48ffa767ff5140727e273
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-04-26 15:11:36 +0900

    第02回 Unity入門

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VSCheck : MonoBehaviour
+public class Goal : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ÉSÅ[Éã");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
